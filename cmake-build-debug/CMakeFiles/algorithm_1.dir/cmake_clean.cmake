1bbcf3dabb9ca3cc2daed1e97546624d57383d18
lyhhhhh <1300833021@qq.com>
2019-11-05 15:32:55 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/algorithm_1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/algorithm_1.dir/main.c.o"
+  "algorithm_1"
+  "algorithm_1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/algorithm_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,93 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#define PORT 8080
+
+char* getrequest(char* buf);// parse the request and get the file name we want.
+
+int main(int argc, char const *argv[])
+{
+    int server_fd, new_socket;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+    char *file_name = NULL;
+    char file_content[1024] = {0};
+
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Forcefully attaching socket to the port 8080
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
+                   &opt, sizeof(opt)))
+    {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons( PORT );
+
+    // Forcefully attaching socket to the port 8080
+    if (bind(server_fd, (struct sockaddr *)&address,
+             sizeof(address))<0)
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 3) < 0)
+    {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+                             (socklen_t*)&addrlen))<0)
+    {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+    read( new_socket , buffer, 1024);
+    file_name = getrequest(buffer);
+    FILE* file = fopen(file_name,"rb");
+    if(file != NULL) {
+        fread(file_content, sizeof(char), 1024, file);
+        send(new_socket , file_content , strlen(file_content) , 0 );
+        fclose(file);
+        printf("request file name: %s\n",file_name);
+    } else {
+        file = fopen("Not_found.html", "rb");
+        fread(file_content, sizeof(char), 1024, file);
+        send(new_socket , file_content, strlen(file_content) , 0 );
+        fclose(file);
+        printf("request file name: %s\n",file_name);
+        printf("the file not found\n");
+    }
+    return 0;
+}
+
+char* getrequest(char* buf) {
+    char request[100];
+    while(*buf != '/') {
+        buf++;
+    }
+    int i = 0;
+    buf++;
+    while(*buf != ' ') {
+        request[i] = *buf;
+        i++;
+        buf++;
+    }
+    request[i]='\0';
+    for(int j = 0; j<100; j++) {
+        buf[j] = request[j];
+    }
+    return buf;
+}
